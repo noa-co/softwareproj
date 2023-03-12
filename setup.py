5a65d826c30e09c9ae8e06eb15ecb3b96561e9bc
noac7 <nc7@mail.tau.ac.il>
2023-03-12 13:13:42 +0200

    fixed setup

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import Extension, setup
 module = Extension("mykmeanssp",
                    sources=[
                        'spkmeans.c',
-                       'spkmeansmodule.c'
+                       'spkmeansmodule.c',
+                       'sp_io_utils.c',
+                       'sp_kmeansalho.c'
                    ])
 setup(name='mykmeanssp',
       version='1.0',
